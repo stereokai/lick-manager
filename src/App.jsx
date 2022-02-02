77b09eb477efe05b8867e697fa0e56536cb31ef8
Stereokai <tom@stereokai.com>
2022-02-02 10:17:57 +0100

    Add tailwind + some ui code

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import AlphaTab from './Alphatab.jsx';
-import './App.css';
-import { Fretboard } from './Fretboard.jsx';
+import AlphaTab from "./Alphatab.jsx"
+import { Fretboard } from "./Fretboard.jsx"
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="flex flex-col min-h-screen bg-red-300">
+      <div className="flex-grow bg-white m-5">
         <AlphaTab />
+      </div>
+      <div className="flex-shrink bg-white m-5">
         <Fretboard />
-      </header>
+      </div>
     </div>
   )
 }
