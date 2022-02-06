e5abdaee90302d2d4f64e7738e7c49a4bacc0121
Stereokai <tom@stereokai.com>
2022-02-06 14:09:24 +0100

    Only show tabbed notes on the fretboard

--- src/Fretboard.jsx
+++ src/Fretboard.jsx
@@ -22,12 +22,37 @@ const useFretboard = (figureRef, opts) => {
   }, [figureRef, opts]);
   return fretboard;
 };
+
+function concatSets(...iterables) {
+  const set = new Set();
+  for (const iterable of iterables) {
+    for (const item of iterable) {
+      set.add(item);
+    }
+  }
+
+  return set;
+}
+
+function dedupeNotes(beats) {
+  if (!beats.length) return [];
+
+  const set = concatSets(...beats.map((beat) => beat.notes));
+  return [...set].map((noteStr) => {
+    const note = noteStr.split(":");
+    return { fret: note[0], string: note[1] };
+  });
+}
+
 const options = { tom: 10 };
 const CLICK_TIMEOUT = 150;
 let lastClick = false;
 const FretboardJSX = ({ dots, setDots }) => {
   const figureRef = useRef(null);
-  const { dispatch } = useBeats();
+  const {
+    dispatch,
+    state: { beats },
+  } = useBeats();
   const fretboard = useFretboard(figureRef, options);
   useEffect(() => {
     try {
@@ -44,14 +69,14 @@ const FretboardJSX = ({ dots, setDots }) => {
         if (lastClick) {
           const { string, fret } = position;
           clearTimeout(lastClick);
-          setDots({ string, fret, type: DOTS_ACTIONS.REMOVE });
+          // setDots({ string, fret, type: DOTS_ACTIONS.REMOVE });
           dispatch({ type: "REMOVE_NOTE", note: position });
           lastClick = false;
           return;
         }
 
         lastClick = setTimeout(() => {
-          setDots({ dot: position, type: DOTS_ACTIONS.ADD });
+          // setDots({ dot: position, type: DOTS_ACTIONS.ADD });
           dispatch({ type: "ADD_NOTE", note: position });
           lastClick = false;
         }, CLICK_TIMEOUT);
@@ -60,7 +85,7 @@ const FretboardJSX = ({ dots, setDots }) => {
   }, [fretboard]);
 
   useEffect(() => {
-    fretboard.setDots(dots).render();
+    fretboard.setDots([...dots, ...dedupeNotes(beats)]).render();
   }, [dots]);
 
   // useEffect(() => {
