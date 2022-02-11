17512479448398c4dd8ec0259070be96f6d3712c
Stereokai <tom@stereokai.com>
2022-02-11 09:04:23 +0100

    Expand Licks to provider

--- src/routes/Licks.jsx
+++ src/routes/Licks.jsx
@@ -1,3 +1,32 @@
+import { createContext, useContext, useMemo, useReducer } from "react";
+
+const LicksContext = createContext();
+
+export const LicksActions = {};
+
+export const licksReducer = (state, action) => {
+  // switch (action.type) {}
+};
+
+export const LicksProvider = (props) => {
+  const [state, dispatch] = useReducer(licksReducer, {});
+  const value = useMemo(() => [state, dispatch], [state]);
+  return <LicksContext.Provider value={value} {...props} />;
+};
+
+export const useLicks = () => {
+  const context = useContext(LicksContext);
+  if (!context) {
+    throw new Error(`useLicks must be used within a LicksProvider`);
+  }
+  const [state, licksDispatch] = context;
+
+  return {
+    state,
+    licksDispatch,
+  };
+};
+
 export default function Licks() {
   return <main>Lick list</main>;
 }
